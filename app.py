36613ddd8af74509eefd32cab0e696b9bf72c886
contatosanth-design <contatosanth@gmail.com>
2026-03-08 09:37:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ st.set_page_config(
     layout="centered"
 )
 
-st.title("🐾 Sistema de Atendimento Veterinário")
+st.title("🐾 Sistema Veterinário")
 
-st.write("Bem-vindo ao sistema de atendimento.")
+st.write("Sistema online funcionando no Render.")
 
 nome = st.text_input("Nome do tutor")
 pet = st.text_input("Nome do pet")
 idade = st.number_input("Idade do pet", min_value=0, max_value=50)
 
 if st.button("Registrar atendimento"):
-    st.success(f"Atendimento registrado para {pet}, tutor {nome}.")
+    st.success(f"Atendimento registrado para {pet} (Tutor: {nome})")
